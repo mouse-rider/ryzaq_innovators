13987637034de59b6dfdec182dd04c144f60e9b1
mouse-rider <kishorekumarips28@gmail.com>
2025-02-18 21:31:14 +0530

    Quanta

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,36 +1,36 @@
-import { Radar } from "lucide-react";
+// import { Radar } from "lucide-react";
 
-interface SponsorProps {
-  icon: JSX.Element;
-  name: string;
-}
+// interface SponsorProps {
+//   icon: JSX.Element;
+//   name: string;
+// }
 
-const sponsors: SponsorProps[] = [
-  {
-    icon: <Radar size={34} />,
-    name: "Sponsor 1",
-  },
-  {
-    icon: <Radar size={34} />,
-    name: "Sponsor 2",
-  },
-  {
-    icon: <Radar size={34} />,
-    name: "Sponsor 3",
-  },
-  {
-    icon: <Radar size={34} />,
-    name: "Sponsor 4",
-  },
-  {
-    icon: <Radar size={34} />,
-    name: "Sponsor 5",
-  },
-  {
-    icon: <Radar size={34} />,
-    name: "Sponsor 6",
-  },
-];
+// const sponsors: SponsorProps[] = [
+//   {
+//     icon: <Radar size={34} />,
+//     name: "Sponsor 1",
+//   },
+//   {
+//     icon: <Radar size={34} />,
+//     name: "Sponsor 2",
+//   },
+//   {
+//     icon: <Radar size={34} />,
+//     name: "Sponsor 3",
+//   },
+//   {
+//     icon: <Radar size={34} />,
+//     name: "Sponsor 4",
+//   },
+//   {
+//     icon: <Radar size={34} />,
+//     name: "Sponsor 5",
+//   },
+//   {
+//     icon: <Radar size={34} />,
+//     name: "Sponsor 6",
+//   },
+// ];
 
 // export const Sponsors = () => {
 //   return (
